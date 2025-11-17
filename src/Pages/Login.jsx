8aa5f27bd17dbe51817bd6715cce7261db6f610f
mdshoaib77 <email4966@gmail.com>
2025-11-17 09:10:40 +0600

    Login chnages

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -64,7 +64,7 @@ const Login = () => {
           className="md:w-96 w-80 flex flex-col items-center justify-center"
           onSubmit={handelLogin}
         >
-          <h2 className="text-4xl  font-medium text-green-500">Log in</h2>
+          <h2 className="text-4xl  font-medium text-purple-800">Log in</h2>
           <p className="text-sm text-gray-500/90 mt-3">
             Welcome back! Please Log in to continue
           </p>
@@ -88,7 +88,7 @@ const Login = () => {
             <div className="w-full h-px bg-gray-300/90"></div>
           </div>
 
-          <div className="flex items-center w-full bg-transparent border border-green-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+          <div className="flex items-center w-full bg-transparent border border-purple-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
             <svg
               width="16"
               height="11"
@@ -100,7 +100,7 @@ const Login = () => {
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M0 .55.571 0H15.43l.57.55v9.9l-.571.55H.57L0 10.45zm1.143 1.138V9.9h13.714V1.69l-6.503 4.8h-.697zM13.749 1.1H2.25L8 5.356z"
-                fill="#22c55e"
+                fill="#6b46c1"
               />
             </svg>
             <input
@@ -114,7 +114,7 @@ const Login = () => {
             />
           </div>
 
-          <div className="flex items-center mt-6 w-full bg-transparent border border-green-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+          <div className="flex items-center mt-6 w-full bg-transparent border border-purple-800/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
             <svg
               width="13"
               height="17"
@@ -124,7 +124,7 @@ const Login = () => {
             >
               <path
                 d="M13 8.5c0-.938-.729-1.7-1.625-1.7h-.812V4.25C10.563 1.907 8.74 0 6.5 0S2.438 1.907 2.438 4.25V6.8h-.813C.729 6.8 0 7.562 0 8.5v6.8c0 .938.729 1.7 1.625 1.7h9.75c.896 0 1.625-.762 1.625-1.7zM4.063 4.25c0-1.406 1.093-2.55 2.437-2.55s2.438 1.144 2.438 2.55V6.8H4.061z"
-                fill="#22c55e
+                fill="#6b46c1
 "
               />
             </svg>
@@ -140,13 +140,13 @@ const Login = () => {
               onClick={() => setShow(!show)}
               className="text-gray-500 mr-3"
             >
-              {show ? <FaRegEyeSlash color="#22c55e"/> : <FaRegEye color="#22c55e"/>}
+              {show ? <FaRegEyeSlash color="#6b46c1"/> : <FaRegEye color="#6b46c1"/>}
             </button>
           </div>
 
           <div className="w-full flex items-center justify-between mt-4 ml-5 text-gray-500/80">
             <div className="flex items-center gap-2">
-              <input className="h-5 accent-green-500" type="checkbox" id="checkbox" required  />
+              <input className="h-5 accent-purple-800" type="checkbox" id="checkbox" required  />
               <label className="text-sm" htmlFor="checkbox">
                 Remember me
               </label>
@@ -155,7 +155,7 @@ const Login = () => {
 
           <button
             type="submit"
-            className="mt-8 w-full h-11 rounded-full text-white bg-green-500 hover:opacity-90 transition-opacity"
+            className="mt-8 w-full h-11 rounded-full text-white bg-purple-800 hover:opacity-90 transition-opacity"
           >
             Login
           </button>
@@ -163,7 +163,7 @@ const Login = () => {
             Don’t have an account?{" "}
             <Link
               to="/sign-up"
-              className="text-green-500 hover:underline"
+              className="text-purple-800 hover:underline"
               href="#"
             >
               Sign up
