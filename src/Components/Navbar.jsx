74cd039c28b777b4ac6da8202f1c2ef7cfbe08e5
mdshoaib77 <email4966@gmail.com>
2025-11-17 15:06:51 +0600

    Navbar chanegrs

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         }
       >
         <Home size={18} />
-        Home
+        Hom
       </NavLink>
       <NavLink to="/all-issues" className={linkClasses}>
         <Calendar size={18} />
